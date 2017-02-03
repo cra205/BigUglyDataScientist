7d3ba3d20f6d36357bdca993d84c7af88b46bc0b
cra205 <abramsdylanb@gmail.com>
2017-02-03 10:37:53 -0600

    Update century_club_yarn.py

--- century_club_yarn.py
+++ century_club_yarn.py
@@ -1,29 +1,3 @@
-﻿# -*- coding: UTF-8 -*-
-#
-# Copyright 2016 MongoDB, Inc.
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# To run this example use:
-# ./bin/spark-submit --master "local[4]"  \
-#                    --conf "spark.mongodb.input.uri=mongodb://127.0.0.1/test.coll?readPreference=primaryPreferred" \
-#                    --conf "spark.mongodb.output.uri=mongodb://127.0.0.1/test.coll" \
-#                    --packages org.mongodb.spark:mongo-spark-connector_2.10:1.0.0 \
-#                    introduction.py
-
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
 
